7dd1f58aae8ba6be9fc708e23ad284ba7f3dd0ac
SpiritSeeker <sanjeet029@gmail.com>
2017-04-23 09:54:47 +0530

    Add files via upload

---
+++ TemperatureI2C.ino
@@ -0,0 +1,26 @@
+#include <Wire.h>
+#define SLAVE_ADDRESS 0x40
+
+byte response[2];
+volatile short reading;
+void setup() {
+  Serial.begin(9600);
+  analogReference(INTERNAL);
+  Wire.begin(SLAVE_ADDRESS);
+  Wire.onRequest(sendData);
+  pinMode(A0,INPUT);
+}
+
+void loop() {
+  //Wire.write(reading);
+  delay(1000);
+}
+
+void sendData(){
+  reading = analogRead(A0);
+  reading = map(reading,0,1023,0,255);
+  response[0]=(byte)reading;
+  Wire.write(response,2);
+}  
+
+
